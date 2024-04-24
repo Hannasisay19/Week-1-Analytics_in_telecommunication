722207eba3f8849530911acd2f1be6cfc65004ab
Hannasisay19 <hannasisay05@gmail.com>
2024-04-24 08:35:18 +0300

    read in df

---
+++ postgres_connection.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def connect_to_postgres():
+
+    database_name = 'telecom'
+    table_name= 'xdr_data'
+
+    connection_params = { "host": "localhost", "user": "postgres", "password": 12345,
+                        "port": "5432", "database": database_name}
+
+    engine = create_engine(f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['database']}")
+
+    return engine
+
+
+if __name__ == "__main__":
+    engine=connect_to_postgres()
+sql_query='SELECT * FROM xdr_data Limit5'
+df=pd.read_sql(sql_query,con=engine)
+print(df)
