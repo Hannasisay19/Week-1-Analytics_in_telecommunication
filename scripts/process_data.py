ad6380c11e34b4886d456ec56d87a66547da1fbd
Hannasisay19 <hannasisay05@gmail.com>
2024-04-24 12:50:41 +0300

    retrieve data from a PostgreSQL

---
+++ scripts/process_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from scripts.postgres_connection import connect_to_postgres
+
+
+def get_df():
+    engine = connect_to_postgres()
+
+    sql_query = 'SELECT * FROM xdr_data'
+    df = pd.read_sql(sql_query, con=engine)
+
+    return df
+
+if __name__ == "__main__":
+
+    df = get_df()
+    
